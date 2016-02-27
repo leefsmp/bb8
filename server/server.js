e2f94894991b01e5e6f82c11c030477116eadda4
Philippe Leefsma <philippe.leefsma@gmail.com>
2016-02-27 18:43:24 +0100

    update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ import express from 'express';
 import IoTAPI from './api/endpoints/IoT';
 import bb8API from './api/endpoints/bb8';
 import IoTSvc from './api/services/IoTSvc';
-import config from '../config/dev.config';
+import config from '../config/prod.config';
 
 var app = express();
 
